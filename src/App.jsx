ee1d05071e13235f768e54b6122bed0a4ece36ed
naim2592 <mddurjoyalok@gmail.com>
2024-10-30 12:35:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-4xl text-pink-500 bg-purple-500'>Lwets Start a new Mission</h1>
+      <button className='btn btn-square'>Its a btn</button>
+    </>
+  )
+}
+
+export default App
